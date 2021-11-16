49413582edd3fec579bdebf83328d9034b842538
Maciej Żądło <mzadlo@student.agh.edu.pl>
2021-11-16 17:48:27 +0100

    all works xDDDD

---
+++ kubrakowo/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Kubrakowo.WebApp.Domain;
+using Kubrakowo.WebApp.Domain.Entities;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(Kubrakowo.WebApp.Areas.Identity.IdentityHostingStartup))]
+namespace Kubrakowo.WebApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
---
+++ kubrakowo/Domain/Configuration/UserConfiguration.cs
@@ -0,0 +1,18 @@
+﻿using Kubrakowo.WebApp.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kubrakowo.WebApp.Domain.Configuration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+
+        }
+    }
+}

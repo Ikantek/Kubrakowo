1f5a90929d6dfa37629a74a450ac2df0186a016d
Maciej Żądło <mzadlo@student.agh.edu.pl>
2021-11-16 17:22:24 +0100

    asd

--- kubrakowo/Domain/Context.cs
+++ kubrakowo/Domain/Context.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Kubrakowo.WebApp.Domain.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using System.Reflection;
 
 namespace Kubrakowo.WebApp.Domain
 {
-    public class Context : DbContext
+    public class Context : IdentityDbContext<User, IdentityRole<int>, int>
     {
         public Context(DbContextOptions<Context> options) : base(options)
         {
---
+++ kubrakowo/Domain/Entities/User.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Kubrakowo.WebApp.Domain.Entities
+{
+    public class User : IdentityUser<int>
+    {
+        public string Name { get; set; }
+        public string Surname { get; set; }
+    }
+}
